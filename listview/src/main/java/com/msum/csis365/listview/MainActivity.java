e47c9d5a436f6a45e4ca334941b59374d731c8c4
Kyle <kdahn@bushelpowered.com>
2020-01-29 20:21:34 -0600

    Added corresponding modules, listview implementation, and gridview implementation

---
+++ listview/src/main/java/com/msum/csis365/listview/MainActivity.java
@@ -0,0 +1,30 @@
+package com.msum.csis365.listview;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.msum.csis365.list_view.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView lvExample;
+    private String[] exampleNames = {"Kyle", "Aishat", "Nathan", "Chris", "Jake", "Ben", "Peter"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Obtain and assign the ListView with Id lv_example in the layout activity_main
+        lvExample = findViewById(R.id.lv_example);
+
+        // Create a generic ArrayAdapter
+        ArrayAdapter exampleAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, exampleNames);
+
+        // Assign exampleAdapter to lvExample
+        lvExample.setAdapter(exampleAdapter);
+    }
+}
