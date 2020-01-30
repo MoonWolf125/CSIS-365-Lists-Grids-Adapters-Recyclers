e6a877d2360eab237c9df0013938970fc4281244
Kyle <kdahn@bushelpowered.com>
2020-01-29 21:31:25 -0600

    Flushed out RecyclerView implementation

---
+++ recyclerview/src/main/java/com/msum/csis365/recyclerview/ExampleRowView.java
@@ -0,0 +1,47 @@
+package com.msum.csis365.recyclerview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class ExampleRowView extends FrameLayout {
+
+    private TextView tvData;
+
+    public ExampleRowView(@NonNull Context context) {
+        super(context);
+        setLayoutParams(new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+
+        // Inflate the custom layout and assign the view to the class-level variable
+        inflate(context, R.layout.view_example_row, this);
+        tvData = findViewById(R.id.tv_data);
+    }
+
+    public ExampleRowView(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        setLayoutParams(new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+
+        // Inflate the custom layout and assign the view to the class-level variable
+        inflate(context, R.layout.view_example_row, this);
+        tvData = findViewById(R.id.tv_data);
+    }
+
+    public ExampleRowView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        setLayoutParams(new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+
+        // Inflate the custom layout and assign the view to the class-level variable
+        inflate(context, R.layout.view_example_row, this);
+        tvData = findViewById(R.id.tv_data);
+    }
+
+    // Set the data to tvData
+    public void setup(String data) {
+        tvData.setText(data);
+    }
+}
